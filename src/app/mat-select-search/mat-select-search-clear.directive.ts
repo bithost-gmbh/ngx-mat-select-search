f5588e8a2dd79b4920f76660c7b56fa1027e0097
Esteban Marin <esteban.marin@bithost.ch>
2018-07-12 09:35:44 +0200

    #41: fix tests, update docs

--- src/app/mat-select-search/mat-select-search-clear.directive.ts
+++ src/app/mat-select-search/mat-select-search-clear.directive.ts
@@ -1,5 +1,12 @@
 import { Directive } from '@angular/core';
 
+/**
+ * Directive for providing a custom clear-icon.
+ * e.g.
+ * <ngx-mat-select-search [formControl]="bankFilterCtrl">
+ *   <mat-icon ngxMatSelectSearchClear>delete</mat-icon>
+ * </ngx-mat-select-search>
+ */
 @Directive({
     selector: '[ngxMatSelectSearchClear]'
 })
