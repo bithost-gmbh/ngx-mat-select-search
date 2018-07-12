e9cde0964ababefb84679f8b6af25fa07acb3d47
Esteban Marin <estebanmarin@gmx.ch>
2018-07-12 08:09:15 +0200

    allow inserting custom elements as clear icon

--- src/app/mat-select-search/mat-select-search-clear.directive.ts
+++ src/app/mat-select-search/mat-select-search-clear.directive.ts
@@ -1,6 +1,6 @@
 import { Directive } from '@angular/core';
 
 @Directive({
-    selector: 'mat-icon[ngxMatSelectSearchClear]'
+    selector: '[ngxMatSelectSearchClear]'
 })
 export class MatSelectSearchClearDirective {}
