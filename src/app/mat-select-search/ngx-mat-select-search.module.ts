652de6306a75919fb22e84c9990f3ae6405caf55
Esteban Gehring <esteban.gehring@bithost.ch>
2020-03-04 10:00:16 +0100

    prepare release 2.1.2

--- src/app/mat-select-search/ngx-mat-select-search.module.ts
+++ src/app/mat-select-search/ngx-mat-select-search.module.ts
@@ -16,7 +16,7 @@ import { CommonModule } from '@angular/common';
 
 import { MatSelectSearchClearDirective } from './mat-select-search-clear.directive';
 
-export const MatSelectSearchVersion = '2.1.1';
+export const MatSelectSearchVersion = '2.1.2';
 
 
 @NgModule({
