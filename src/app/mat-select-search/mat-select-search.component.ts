fbfe2d91fd6d5a79dafc434e4b91c549e6c4c5f1
Robert Pattis <pattis_r@hotmail.de>
2020-03-04 09:54:17 +0100

    fix selection of different instances of same object when using `multiple` (#216)
    * #215: fix selection of different instances of same object when using `multiple`
    
    * Apply suggestions from code review
    
    Co-authored-by: Esteban Gehring <esteban.gehring@gmail.com>

--- src/app/mat-select-search/mat-select-search.component.ts
+++ src/app/mat-select-search/mat-select-search.component.ts
@@ -550,7 +550,8 @@ export class MatSelectSearchComponent implements OnInit, OnDestroy, AfterViewIni
             }
             const optionValues = this.matSelect.options.map(option => option.value);
             this.previousSelectedValues.forEach(previousValue => {
-              if (values.indexOf(previousValue) === -1 && optionValues.indexOf(previousValue) === -1) {
+              if (!values.some(v => this.matSelect.compareWith(v, previousValue))
+                && !optionValues.some(v => this.matSelect.compareWith(v, previousValue))) {
                 // if a value that was selected before is deselected and not found in the options, it was deselected
                 // due to the filtering, so we restore it.
                 values.push(previousValue);
