bcba112ac26940d373546a2c915c225d07cb5d24
Esteban Gehring <esteban.gehring@bithost.ch>
2020-04-29 08:27:26 +0200

    release of 2.2.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,13 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSelectModule } from '@angular/material/select';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import {MatTooltipModule} from '@angular/material/tooltip';
 
 import { NgxMatSelectSearchModule } from './mat-select-search/ngx-mat-select-search.module';
 
@@ -32,8 +31,7 @@ import { TooltipSelectAllExampleComponent } from './examples/07-tooltip-select-a
     MatFormFieldModule,
     MatIconModule,
     MatSelectModule,
-    MatToolbarModule,
-    MatTooltipModule
+    MatToolbarModule
   ]
 })
 export class MaterialModule {}
--- src/app/mat-select-search/ngx-mat-select-search.module.ts
+++ src/app/mat-select-search/ngx-mat-select-search.module.ts
@@ -17,7 +17,7 @@ import { CommonModule } from '@angular/common';
 
 import { MatSelectSearchClearDirective } from './mat-select-search-clear.directive';
 
-export const MatSelectSearchVersion = '2.1.2';
+export const MatSelectSearchVersion = '2.2.0';
 
 
 @NgModule({
