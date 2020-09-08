c3c71262c10b53157f8fb1c4b28d2e68db10be96
Esteban Gehring <esteban.gehring@bithost.ch>
2020-09-08 13:15:33 +0200

    chore: add right-to-left mode to example page
    #258

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSelectModule } from '@angular/material/select';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import {MatSelectInfiniteScrollModule} from 'ng-mat-select-infinite-scroll';
+import { MatSelectInfiniteScrollModule } from 'ng-mat-select-infinite-scroll';
 
 
 import { NgxMatSelectSearchModule } from './mat-select-search/ngx-mat-select-search.module';
@@ -19,11 +19,10 @@ import { MultipleSelectionExampleComponent } from './examples/02-multiple-select
 import { CustomClearIconExampleComponent } from './examples/03-custom-clear-icon-example/custom-clear-icon-example.component';
 import { OptionGroupsExampleComponent } from './examples/04-option-groups-example/option-groups-example.component';
 import { ServerSideSearchExampleComponent } from './examples/05-server-side-search-example/server-side-search-example.component';
-import {
-  MultipleSelectionSelectAllExampleComponent
-} from './examples/06-multiple-selection-select-all-example/multiple-selection-select-all-example.component';
+import { MultipleSelectionSelectAllExampleComponent } from './examples/06-multiple-selection-select-all-example/multiple-selection-select-all-example.component';
 import { TooltipSelectAllExampleComponent } from './examples/07-tooltip-select-all-example/tooltip-select-all-example.component';
 import { InfiniteScrollExampleComponent } from './examples/08-infinite-scroll-example/infinite-scroll-example.component';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 
 /**
@@ -49,7 +48,8 @@ export class MaterialModule {}
     ReactiveFormsModule,
     BrowserAnimationsModule,
     NgxMatSelectSearchModule,
-    MatSelectInfiniteScrollModule
+    MatSelectInfiniteScrollModule,
+    MatSlideToggleModule
   ],
   declarations: [
     AppComponent,
