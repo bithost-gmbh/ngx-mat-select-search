fb7ee95bd63d85d443dbb2f6333e2f0cef4cd919
Esteban Gehring <esteban.gehring@bithost.ch>
2022-07-19 08:13:11 +0200

    Revert "[FIX] Update previous selected values in mode multi (#377)" (#388)
    This reverts commit 1930d9a937aac41406e50a51e9540c86d5465a9c.

--- src/app/mat-select-search/mat-select-search.component.ts
+++ src/app/mat-select-search/mat-select-search.component.ts
@@ -605,31 +605,30 @@ export class MatSelectSearchComponent implements OnInit, OnDestroy, ControlValue
 
     this.matSelect.ngControl.valueChanges
       .pipe(takeUntil(this._onDestroy))
-      .subscribe((selectedValues) => {
+      .subscribe((values) => {
         let restoreSelectedValues = false;
-        const updatedSelectedValues = selectedValues && Array.isArray(selectedValues)
-          ? [...selectedValues]
-          : [];
-
         if (this.matSelect.multiple) {
           if ((this.alwaysRestoreSelectedOptionsMulti || (this._formControl.value && this._formControl.value.length))
             && this.previousSelectedValues && Array.isArray(this.previousSelectedValues)) {
+            if (!values || !Array.isArray(values)) {
+              values = [];
+            }
             const optionValues = this.matSelect.options.map(option => option.value);
             this.previousSelectedValues.forEach(previousValue => {
-              if (!updatedSelectedValues.some(v => this.matSelect.compareWith(v, previousValue))
+              if (!values.some(v => this.matSelect.compareWith(v, previousValue))
                 && !optionValues.some(v => this.matSelect.compareWith(v, previousValue))) {
                 // if a value that was selected before is deselected and not found in the options, it was deselected
                 // due to the filtering, so we restore it.
-                updatedSelectedValues.push(previousValue);
+                values.push(previousValue);
                 restoreSelectedValues = true;
               }
             });
           }
         }
-        this.previousSelectedValues = updatedSelectedValues;
+        this.previousSelectedValues = values;
 
         if (restoreSelectedValues) {
-          this.matSelect._onChange(updatedSelectedValues);
+          this.matSelect._onChange(values);
         }
       });
   }
