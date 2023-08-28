ff77f1c7c138bfc9372b762863e3a1b387f177c9
Esteban Gehring <esteban.gehring@bithost.ch>
2023-08-28 20:00:44 +0200

    #431: fix background issue with angular material 16, prepare release 7.0.3

--- src/app/mat-select-search/ngx-mat-select-search.module.ts
+++ src/app/mat-select-search/ngx-mat-select-search.module.ts
@@ -19,7 +19,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { MatSelectNoEntriesFoundDirective } from './mat-select-no-entries-found.directive';
 import { MatDividerModule } from '@angular/material/divider';
 
-export const MatSelectSearchVersion = '7.0.2';
+export const MatSelectSearchVersion = '7.0.3';
 export { MatSelectSearchClearDirective };
 export { MatSelectNoEntriesFoundDirective };
 
