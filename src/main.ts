d9206b0757fb8bc1c0bbca4b0b7fe1b26d09dfd6
Esteban Gehring <esteban.gehring@gmail.com>
2025-05-29 12:52:29 +0200

    Update src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { environment } from './environments/environment';
 import { AppComponent } from './app/app.component';
