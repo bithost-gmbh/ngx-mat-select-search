179e8698ac3c3e846077002d562dc31094ab11b7
Lehoczky Zoltán <ifjlehoczkyzoltan@gmail.com>
2026-07-27 11:52:16 +0200

    update repository's Angular version to v21 (#621)
    * update repository's Angular version to v21
    
    * fix lint errors
    
    * Delete package-lock.json
    
    * chore: regenerate package-lock.json
    
    ---------
    
    Co-authored-by: Esteban Gehring <esteban.gehring@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
+import { provideZoneChangeDetection } from "@angular/core";
 import { bootstrapApplication } from "@angular/platform-browser";
-import { provideAnimations } from "@angular/platform-browser/animations";
 
 import { AppComponent } from "./app/app.component";
 
 bootstrapApplication(AppComponent, {
-  providers: [provideAnimations()],
+  providers: [provideZoneChangeDetection()],
 }).catch((err) => console.log(err));
